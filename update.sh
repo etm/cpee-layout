42ceb2b982cadf1c71c3dbd9f850341f7b56342c
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-10-16 16:12:08 +0200

    cleanup

---
+++ update.sh
@@ -0,0 +1,6 @@
+cp ../cpee/cockpit/js/wfadaptor.js .
+cp ../cpee/cockpit/css/wfadaptor.css .
+cp -r ../cpee/cockpit/themes/base.js themes/.
+cp -r ../cpee/cockpit/themes/extended themes/.
+cp -r ../cpee/cockpit/themes/preset themes/.
+cp -r ../cpee/cockpit/themes/packed themes/.
