5b9de0b30a842696055a796c4e49eec54dbe58a4
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2025-12-08 21:30:34 +0100

    presetaltid

--- update.sh
+++ update.sh
@@ -6,5 +6,6 @@ cp ../cpee/cockpit/css/extended_columns-svg.css .
 cp -r ../cpee/cockpit/themes/base.js themes/.
 cp -r ../cpee/cockpit/themes/extended themes/.
 cp -r ../cpee/cockpit/themes/preset themes/.
+cp -r ../cpee/cockpit/themes/presetaltid themes/.
 cp -r ../cpee/cockpit/themes/packed themes/.
 cp -r ../cpee/cockpit/themes/dataflow themes/.
